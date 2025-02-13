776b42e862f7de5065df0b221bde854524987595
Jasmine Zou <jasmine_pyz@icloud.com>
2025-02-13 10:23:17 -0800

    renamed journalEntry to journalEntries

--- src/main/model/Journal.java
+++ src/main/model/Journal.java
@@ -1,39 +1,36 @@
-package model; 
+package model;
 
 import java.time.LocalDateTime;
 import java.util.*;
 
-public class Journal{
-    List<JournalEntry> journal;  
+public class Journal {
+    private List<JournalEntry> journalEntries;
 
-    public Journal(){
-        journal = new ArrayList<JournalEntry>(); 
+    public Journal() {
+        journalEntries = new ArrayList<JournalEntry>();
     }
 
     // REQUIRE: entry does not already exist in the journal
     // MODIFY: this
     // EFFECT: add a new entry to the existing journal
-    public void addEntry(JournalEntry entry){
+    public void addEntry(JournalEntry entry) {
     }
 
-    // REQUIRE: journal is not empty && this entry is in the journal 
-    // MODIFY: this 
-    // EFFECT: delete an entry 
-    public void deleteEntry(LocalDateTime dateTime){
+    // REQUIRE: journal is not empty && this entry is in the journal
+    // MODIFY: this
+    // EFFECT: delete an entry
+    public void deleteEntry(LocalDateTime dateTime) {
     }
 
-    // REQUIRE: journal is not empty && this entry is in the journal 
-    // EFFECT: return the content of the journal along with its text analysis 
-    public JournalEntry getEntry(LocalDateTime dateTime){
+    // REQUIRE: journal is not empty && this entry is in the journal
+    // EFFECT: return the content of the journal along with its text analysis
+    public JournalEntry getEntry(LocalDateTime dateTime) {
         return new JournalEntry(dateTime, "");
     }
 
     // EFFECT: return the preview of all journal entries in the journal
-    public List<String> getAllEntries(Journal journal){
+    public List<String> getAllEntries(Journal journal) {
         return new ArrayList<>();
     }
-    
+
 }
-
-
-
